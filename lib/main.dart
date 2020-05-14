9c7fa34ab3860e6fa5ae16861b08f9cf849e9999
renny langat <rennylngt@gmail.com>
2020-05-14 12:29:55 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:expense_tracker/widgets/new_transaction.dart';
+
+import './models/transaction.dart';
+import './widgets/transactions_list.dart';
+import 'package:flutter/material.dart';
+import './widgets/chart.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        errorColor: Colors.red,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            headline6:TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            button: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6:TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            button: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          ),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    // Transaction(
+    //   id: 't1', 
+    //   title: 'Shoes', 
+    //   amount: 45.6, 
+    //   date: DateTime.now()
+    //   ),
+    // Transaction(
+    //     id: 't2', 
+    //     title: 'Groceries', 
+    //     amount: 78.5, 
+    //     date: DateTime.now()
+    //     )
+  ];
+  
+  List<Transaction> get _recentTransactions{
+    return _userTransaction.where((tx){
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+          )
+        );
+    }).toList();
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount,DateTime chosenDate) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: chosenDate,
+      id: DateTime.now().toString(),
+    );
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+  void _deleteTransaction(String id){
+    setState(() {
+      _userTransaction.removeWhere((tx){
+        return tx.id==id;
+      });
+    });
+
+  }
+  void _startModalBottomSheet(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.white, size: 30),
+            onPressed: () => _startModalBottomSheet(context),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Chart(_recentTransactions),
+            Transactionist(_userTransaction,_deleteTransaction)
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startModalBottomSheet(context),
+      ),
+    );
+  }
+}
